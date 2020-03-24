7c95cc82c24ffbfa07068c9597a94ea832e0dc09
Maciej Szymczyk <zorteran@gmail.com>
2020-03-24 20:37:25 +0100

    added Dead Letter Queue

---
+++ src/main/java/wiadrodanych/streams/handlers/MyDeserializationExceptionHandler.java
@@ -0,0 +1,28 @@
+package wiadrodanych.streams.handlers;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.streams.errors.DeserializationExceptionHandler;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import wiadrodanych.streams.queues.DeadLetterQueue;
+
+import java.util.Map;
+
+public class MyDeserializationExceptionHandler implements DeserializationExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(MyDeserializationExceptionHandler.class);
+
+    @Override
+    public DeserializationHandlerResponse handle(ProcessorContext context, ConsumerRecord<byte[], byte[]> record, Exception exception) {
+        log.warn("Exception caught during Deserialization, taskId: {}, topic: {}, partition: {}, offset: {}", new Object[]{context.taskId(), record.topic(), record.partition(), record.offset(), exception});
+        DeadLetterQueue dlq = DeadLetterQueue.getInstance();
+        dlq.send(record.key(), record.value(), record.headers(), exception.getMessage());
+        return DeserializationHandlerResponse.CONTINUE;
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
---
+++ src/main/java/wiadrodanych/streams/queues/DeadLetterQueue.java
@@ -0,0 +1,60 @@
+package wiadrodanych.streams.queues;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import wiadrodanych.streams.models.Person;
+
+import java.util.Properties;
+
+
+public class DeadLetterQueue {
+    private static final Logger log = LoggerFactory.getLogger(DeadLetterQueue.class);
+
+    private KafkaProducer<String, String> dlqProducer;
+    private static DeadLetterQueue deadLetterQueueInstance;
+    private String dlqTopic = "dead_letter_queue";
+
+    public static DeadLetterQueue getInstance() {
+        if (deadLetterQueueInstance == null) {
+            deadLetterQueueInstance = new DeadLetterQueue();
+        }
+        return deadLetterQueueInstance;
+    }
+
+    private DeadLetterQueue() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:29092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        this.dlqProducer = new KafkaProducer<>(props);
+    }
+
+    public void Send(String key, Person person, Headers headers, String reason) {
+
+    }
+
+    public void send(String key, String value, Headers headers, String reason) throws KafkaException {
+        headers.add(new RecordHeader("failure.reason", reason.getBytes()));
+        headers.add(new RecordHeader("failure.time", String.valueOf(System.currentTimeMillis()).getBytes()));
+
+        log.warn("Sending to Dead Letter Queue {}: {}", dlqTopic, reason);
+
+        dlqProducer.send(new ProducerRecord<>(
+                dlqTopic,
+                null,
+                key,
+                value,
+                headers)
+        );
+    }
+
+    public void send(byte[] key, byte[] value, Headers headers, String reason) throws KafkaException {
+
+        send(new String(key), new String(value), headers, reason);
+    }
+}
